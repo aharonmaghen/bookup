ed40e1296362ca7f3e9d2c86d5f220e1b74e272b
Aharon <aharon@aharon.com>
2019-11-03 23:07:30 -0500

    Added ISBN field to book class

--- src/main/java/bookup/Book.java
+++ src/main/java/bookup/Book.java
@@ -2,5 +2,14 @@ package bookup;
 
 /** Represents a single Book */
 class Book {
+	private String ISBN;
+
+	public String getISBN(){
+		return ISBN;
+	}
+
+	public void setISBN(String ISBN){
+		this.ISBN = ISBN;
+	}
 
 }
