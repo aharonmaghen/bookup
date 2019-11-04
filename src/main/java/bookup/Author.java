5d272e6bd7051809349994caf65449387479376d
Aharon <aharon@aharon.com>
2019-11-04 01:01:44 -0500

    Added fields and getters/setters to Book class. Created new Author class with fields and getters/setters

---
+++ src/main/java/bookup/Author.java
@@ -0,0 +1,47 @@
+package bookup;
+
+/** Stores author information  */
+class Author {
+
+	private String firstName;
+	private String lastName;
+	private String middleInitial; // String since an author can have more than one middle initial
+
+	public Author(String firstName, String lastName, String middleInitial) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.middleInitial = middleInitial;
+	}
+
+	// firstName getter/setter
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName.toLowerCase();
+		this.firstName.charAt(0) -= 32; // keeps firstName with first letter capitalized ex. "Mark"
+	}
+
+	// lastName getter/setter
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+		this.lastName.charAt(0) -= 32; // keeps lastName with first letter capitalized ex. "Twain"
+	}
+
+	// middleInitial getter/setter
+	public String getMiddleInitial() {
+		return middleInitial;
+	}
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial.toUpperCase();
+	}
+
+	@Override
+	public String toString() {
+		return firstName + " " + middleInitial + " " + lastName;
+	}
+
+}
--- src/main/java/bookup/Book.java
+++ src/main/java/bookup/Book.java
@@ -3,13 +3,62 @@ package bookup;
 /** Represents a single Book */
 class Book {
 	private String ISBN;
+	private String title;
+	Author author;
+	private int pages;
+	private int year;
 
-	public String getISBN(){
-		return ISBN;
+	public Book(String ISBN) {
+		this.ISBN = ISBN;
 	}
 
-	public void setISBN(String ISBN){
+	// ISBN getter/setter
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String ISBN) {
 		this.ISBN = ISBN;
+	}
+
+	// title getter/setter
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	// author getter/setter
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor() {
+		this.author = new Author();
+	}
+	public void setAuthor(String firstName, String lastName, String middleInitial) {
+		this.author = new Author(firstName, lastName, middleInitial);
+	}
+
+	// pages getter/setter
+	public int getPages() {
+		return pages;
+	}
+	public void setPages() {
+		this.pages = pages;
+	}
+
+	// year getter/setter
+	public int getYear() {
+		return year;
+	}
+	public void setYear() {
+		this.year = year;
+	}
+
+	@Override
+	public String toString() {
+		return "ISBN: " + ISBN + "\nTITLE: " + title + "\nAUTHOR: " + author.toString() + 
+		"\nPAGES: " + pages + "\nYEAR: " + year;
 	}
 
 }
