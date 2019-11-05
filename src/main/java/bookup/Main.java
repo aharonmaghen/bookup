f376378cf1ef8eda7d71ecb1de2c1eb922bc7ee4
Aharon <aharon@aharon.com>
2019-11-04 23:03:38 -0500

    Fix merge

--- src/main/java/bookup/Main.java
+++ src/main/java/bookup/Main.java
@@ -4,5 +4,9 @@ package bookup;
 class Main {
 	public static void main(String[] args) {
 		System.out.println("Success!!!!!!!!!!");
+		Book book = new Book("1234567890");
+		book.setAuthor(new Author("Aharon Maghen"));
+		book.setPages(250);
+		System.out.println(book);
 	}
 }
