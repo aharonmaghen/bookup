05a6c426453456e1c5ac529bc1d45699be5a1f9e
Aharon <aharon@aharon.com>
2019-12-09 16:17:37 -0500

    Update SQLHelper. Add methods to add and get books from database

--- src/main/java/bookup/Main.java
+++ src/main/java/bookup/Main.java
@@ -1,12 +1,27 @@
 package bookup;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /** Main Bookup Application */
 class Main {
 	public static void main(String[] args) {
 		System.out.println("Success!!!!!!!!!!");
-		Book book = new Book("1234567890");
-		book.setAuthor(new Author("Aharon Maghen"));
-		book.setPages(250);
+		Book book = new Book("987654321");
+		book.setTitle("Building Java Programs");
+		book.setAuthor(new Author("John Doe"));
+		book.setPages(267);
+		book.setYear(1998);
+		try {
+			book.setImageURL(new URL("google.com"));
+		} catch (MalformedURLException e) {
+			System.out.println(e);
+		}
 		System.out.println(book);
+
+		SQLHelper sqlHelper = new SQLHelper();
+
+//		sqlHelper.add(book);
+		System.out.println("Fetching book with isbn = " + book.getISBN() + " -- " + sqlHelper.getBook(book.getISBN()));
 	}
 }
--- src/main/java/bookup/SQLHelper.java
+++ src/main/java/bookup/SQLHelper.java
@@ -1,6 +1,68 @@
 package bookup;
+import javax.xml.transform.Result;
+import java.sql.*;
 
 /** Provides helper methods for interacting with the database */
 class SQLHelper {
-	
+
+    /** Adds specified book to database */
+    public void add(Book book) {
+        Connection con = connect();
+        try {
+            Statement statement = con.createStatement();
+            String SQLAddStatement = String.format("insert into books values(%s, '%s', '%s', %s, %s, %s);", book.getISBN(), book.getTitle(), book.getAuthor(),book.getPages(), book.getYear(), book.getImageURL());
+            System.out.println(SQLAddStatement);
+            statement.executeUpdate(SQLAddStatement);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    /** Returns a Book with the specified isbn or null if the book does not exist in the database */
+    public Book getBook(String isbn) {
+        Connection con = connect();
+        ResultSet results = null;
+        try {
+            Statement statement = con.createStatement();
+            results = statement.executeQuery(String.format("select * from books where isbn = %s", isbn));
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return createBook(results);
+    }
+
+    /** Creates a Book object form the first row of a ResultSet */
+    private static Book createBook(ResultSet results) {
+        if (results == null) return null;
+        Book book = null;
+        try {
+            results.next();
+            book = new Book(results.getString(1));
+            book.setTitle(results.getString(2));
+            book.setAuthor(new Author(results.getString(3)));
+            book.setPages(results.getInt(4));
+            book.setYear(results.getInt(5));
+            book.setImageURL(results.getURL(6));
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return book;
+    }
+
+    /** Returns a Connection that connects to database */
+    private static Connection connect() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BookDatabase?useSSL=false", "root", "");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
+        return con;
+    }
 }
