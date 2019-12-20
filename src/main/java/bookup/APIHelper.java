753dfda2dfe2fcb9aa07c97081383a83e0951943
aharonmaghen <aharon.maghen27@students.kbcc.cuny.edu>
2019-12-20 01:28:11 -0500

    Add get method in APIHelper to get a Book object using the API

--- src/main/java/bookup/APIHelper.java
+++ src/main/java/bookup/APIHelper.java
@@ -1,6 +1,36 @@
 package bookup;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /** Provides methods for interacting with the OpenLibrary API */
 class APIHelper {
 
+    /** Returns a Book object with data of the book specified by the ISBN.
+     * The data is obtained form a book API.
+     */
+    public static void get(String ISBN) throws IOException {
+        URL urlForGetRequest = new URL(String.format("https://openlibrary.org/api/books?bibkeys=ISBN:%s", ISBN));
+        String readLine = null;
+        HttpURLConnection connection = (HttpURLConnection) urlForGetRequest.openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            while ((readLine = in.readLine()) != null) {
+                response.append(readLine);
+            }
+            in.close();
+            // print result
+            System.out.println("JSON String Result: " + response.toString());
+            //GetAndPost.POSTRequest(response.toString());
+        } else {
+            System.out.println("GET NOT WORKED");
+        }
+    }
 }
